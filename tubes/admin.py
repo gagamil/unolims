59f029a68bf057cf428a00ce8809e252dee9069c
Gabriel Gamil <gamil.gabriel@gmail.com>
2021-09-25 00:24:58 +0300

    Add models to admin

--- tubes/admin.py
+++ tubes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from  .models import Tube, TubeBatch, TubeBatchPosition
+
+admin.site.register(Tube)
+admin.site.register(TubeBatch)
+admin.site.register(TubeBatchPosition)
