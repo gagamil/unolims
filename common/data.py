370dee98e2d2c38f9efce3bcf33ce8d793d57693
Gabriel Gamil <gamil.gabriel@gmail.com>
2022-01-09 00:28:31 +0300

    Add common python module to hold stuff used by various apps - coupling only here

---
+++ common/data.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+from typing import List
+from dataclasses_json import dataclass_json
+
+
+@dataclass
+class TubePositionData:
+    barcode: str
+    position: str
+
+@dataclass_json
+@dataclass
+class TubesBatchData:
+    '''
+    - batch_id: equals rack ID
+    - timestamp: ISO str
+    '''
+    batch_type: str
+    batch_id: str
+    timestamp: str
+    tubes: List[TubePositionData]
---
+++ common/signals.py
@@ -0,0 +1,8 @@
+import django.dispatch
+
+from common.data import TubesBatchData
+
+tube_batch_import_done = django.dispatch.Signal()
+def sig_send__tube_batch_data_import_done(*, sender, sender_pk: int, tube_batch_data: TubesBatchData):
+    print(sender, sender_pk, tube_batch_data)
+    tube_batch_import_done.send(sender=sender, sender_pk=sender_pk, tube_batch_data=tube_batch_data)
