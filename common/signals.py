0f9a1a78a57aafbf4f8fcb2a58ed7410efa72375
Gabriel Gamil <gamil.gabriel@gmail.com>
2022-01-09 08:30:28 +0300

    Update type hints for signal send wrapper

--- common/signals.py
+++ common/signals.py
@@ -3,6 +3,6 @@ import django.dispatch
 from common.data import TubesBatchData
 
 tube_batch_import_done = django.dispatch.Signal()
-def sig_send__tube_batch_data_import_done(*, sender, sender_pk: int, tube_batch_data: TubesBatchData):
+def sig_send__tube_batch_data_import_done(*, sender, sender_pk: int, tube_batch_data: TubesBatchData) -> None:
     print(sender, sender_pk, tube_batch_data)
     tube_batch_import_done.send(sender=sender, sender_pk=sender_pk, tube_batch_data=tube_batch_data)
