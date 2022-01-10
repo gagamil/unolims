6746c2981290ec4cadaa85341bb51cd9c2ed7927
Gabriel Gamil <gamil.gabriel@gmail.com>
2022-01-10 11:35:10 +0300

    Add actstream app

--- tubes/apps.py
+++ tubes/apps.py
@@ -6,4 +6,8 @@ class TubesConfig(AppConfig):
     name = 'tubes'
 
     def ready(self):
+        from actstream import registry
+        registry.register(self.get_model('Tube'))
+        registry.register(self.get_model('TubeBatch'))
+        registry.register(self.get_model('TubeBatchPosition'))
         import tubes.handlers
