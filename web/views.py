307da6a33388a256b8e3312e55a4bbca5a5b3bb9
Gabriel Gamil <gamil.gabriel@gmail.com>
2022-01-29 20:55:40 +0300

    Add xtra context to support js app

--- web/views.py
+++ web/views.py
@@ -52,6 +52,13 @@ class TubeBatchListView(LoginRequiredMixin, FilterView):
 class TubeBatchDetailView(LoginRequiredMixin, DetailView):
     model = TubeBatch
 
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['js_data'] = {'title':self.object.title, 'id':self.object.pk}
+        return context
+
 
 class TubeBatchFileImportCreateView(LoginRequiredMixin, CreateView):
     model = FileImportTubeBatch
